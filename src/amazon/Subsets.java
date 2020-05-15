f4ce3be48fcaa023c392a4d0cebceb39d58e3b61
TechBuzz1992 <techbuzz1992@outlook.com>
2020-05-15 10:39:50 +0530

    commit

---
+++ src/amazon/Subsets.java
@@ -0,0 +1,32 @@
+package amazon;
+
+import java.util.*;
+
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+        List<List<Integer>> subsets = getSubsets(nums);
+        for(List<Integer> subset : subsets){
+            System.out.println(subset);
+        }
+        
+    }
+
+    public static List<List<Integer>> getSubsets(int[] nums){
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateSubSets(0, nums,new ArrayList<Integer>(), subsets);
+        return subsets;
+
+    }
+
+    public static void generateSubSets(int index, int[] nums, List<Integer> current, List<List<Integer>> subsets){
+
+        subsets.add(new ArrayList<Integer>(current));
+        for(int i = index;i<nums.length;i++){
+            current.add(nums[i]);
+            generateSubSets(i+1, nums, current, subsets);
+            current.remove(current.size()-1);
+        }
+    }
+    
+}
