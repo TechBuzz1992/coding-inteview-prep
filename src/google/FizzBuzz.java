c5fd4ff02f08dd5b7b0b8fc7d1a41f3a61130718
TechBuzz1992 <techbuzz1992@outlook.com>
2020-05-24 16:32:46 +0530

    commit

--- src/google/FizzBuzz.java
+++ src/google/FizzBuzz.java
@@ -1,6 +1,6 @@
 package google;
 
-import java.util.*;
+//import java.util.*;
 
 public class FizzBuzz {
     public static void main(String[] args) {
--- src/google/NoOfIslands.java
+++ src/google/NoOfIslands.java
@@ -1,6 +1,6 @@
 package google;
 
-import java.util.*;
+//import java.util.*;
 
 public class NoOfIslands {
     public static void main(String[] args) {
