274276ae0b032b19a5fba902323eb48c0cb603d1
TechBuzz1992 <techbuzz1992@outlook.com>
2020-05-24 11:22:14 +0530

    commit

---
+++ src/google/FizzBuzz.java
@@ -0,0 +1,29 @@
+package google;
+
+import java.util.*;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz obj = new FizzBuzz();
+        obj.fizzBuzz(15);
+
+    }
+
+    public void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FizzBuzz");
+
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+        return;
+    }
+
+}
